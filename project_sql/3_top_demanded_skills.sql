05d639aa2b9dbb8847e0d815d667ad408beee76d
Lucas Majerus <lucas.majerus@gmail.com>
2024-11-11 17:26:27 +0100

    update sql files

--- project_sql/3_top_demanded_skills.sql
+++ project_sql/3_top_demanded_skills.sql
@@ -1,5 +1,5 @@
 /*
-Question: What are the most in-demand skills for data analysts?
+Question: What are the most in-demand skills for data analysts ?
 - Join job postings to inner join table similar to query 2
 - Identify the top 5 in-demand skills for a data analyst.
 - Focus on all job postings.
