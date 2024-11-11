8879cf2ae9869d8d39e9433e30277b3aa6d0b555
Lucas Majerus <lucas.majerus@gmail.com>
2024-11-11 16:53:42 +0100

    sql modifs

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -14,10 +14,8 @@ SELECT
 	job_schedule_type,
 	salary_year_avg,
 	job_posted_date
-	-- name AS company_name
 FROM
 	job_postings_fact
--- LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
 WHERE
 	job_title = 'Data Analyst'
 	AND salary_year_avg IS NOT NULL
