07638cfcc8b2fb54f7dd3508d82dec704713b0e2
Lucas Majerus <lucas.majerus@gmail.com>
2024-11-11 16:39:43 +0100

    upload sql files

--- project_sql/2_top_paying_job_skills.sql
+++ project_sql/2_top_paying_job_skills.sql
@@ -1,3 +1,11 @@
+/*
+Question: What skills are required for the top-paying data analyst jobs?
+- Use the top 10 highest-paying Data Analyst jobs from first query
+- Add the specific skills required for these roles
+- Why? It provides a detailed look at which high-paying jobs demand certain skills, 
+    helping job seekers understand which skills to develop that align with top salaries
+*/
+
 -- Gets the top 10 paying Data Analyst jobs 
 WITH top_paying_jobs AS (
     SELECT
